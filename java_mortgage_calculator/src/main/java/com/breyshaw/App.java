531e2c2c9a6fef567b74764232e67ca4b2507892
breyshaw <dawsonbrey@gmail.com>
2022-02-21 13:16:22 -0500

    solution, add notes

--- java_mortgage_calculator/src/main/java/com/breyshaw/App.java
+++ java_mortgage_calculator/src/main/java/com/breyshaw/App.java
@@ -1,21 +1,33 @@
 package com.breyshaw;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
+    //Declare constants to make it more readable to others
+        final byte  MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
     Scanner scanner = new Scanner(System.in);
+
     System.out.print("Principal:");
-    String principal = scanner.nextLine();
-    System.out.println(principal);
-    System.out.print("Annual Interest Rate:");
-    String annual_interest_rate = scanner.nextLine();
-    System.out.println(annual_interest_rate);
-    System.out.print("Period (Years):");
-    String period = scanner.nextLine();
-    System.out.println(period); //printing these for now to test the inputs
-    //After taking this info I need to calculate the monthly payments
+    int principal = scanner.nextInt();
+    //int allows storing of a value up to 2 billion. When using scanner, next() has to match the variable type, nextInt, nextFloat, etc.
+    System.out.print("Annual Interest Rate: ");
+    float annualInterest = scanner.nextFloat();
+    float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR; //Again, thest constant look alot better to other programmers than unlabled numbers
+    System.out.print("Period (Years): ");
+    byte years = scanner.nextByte(); // a byte is sufficient here
+    int numberOfPayments = years * MONTHS_IN_YEAR;
+
+    double mortgage = principal * 
+    (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) 
+    / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+    System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
